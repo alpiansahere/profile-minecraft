f5794cb8be2b6946b01071b4d1d61f45e39633d7
alpiansahere <ridoalpiansah8@gmail.com>
2025-02-13 19:00:44 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,17 +9,3 @@ function updateClock() {
 
 setInterval(updateClock, 1000); // Perbarui setiap 1 detik
 updateClock(); // Panggil langsung saat halaman dimuat
-
-const text = "GamerTag :"; // Ganti dengan gamertag kamu
-const typingContainer = document.querySelector(".typing-container");
-let index = 0;
-
-function typeEffect() {
-    if (index < text.length) {
-        typingContainer.textContent += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 1000);
-    }
-}
-
-typeEffect();
